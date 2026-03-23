25b37265259b25babed4f208fc69f0b37eb1f075
foreverzzzzzzz <15834063946@163.com>
2026-03-23 19:11:28 +0800

    First commit

---
+++ Assets/PLAYER TWO/Platformer Project/Scripts/Entity/Entity.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EntityBase : MonoBehaviour
+{
+    
+}
+
+public abstract class Entity<T> : EntityBase where T : EntityBase
+{
+    public int a;
+    public int b;
+
+}
---
+++ Assets/PLAYER TWO/Platformer Project/Scripts/Entity/EntityState.cs
@@ -0,0 +1,5 @@
+﻿
+public abstract class EntityState<T> where T : Entity<T>
+{
+    
+}
